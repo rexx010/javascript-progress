dce6598ebfde14bbde211636385f1ef5e04a0ec9
rexx010 <grexx200@gmail.com>
2025-05-21 12:04:13 +0100

    creating of object in js

---
+++ objOfPerson.js
@@ -0,0 +1,25 @@
+//create
+const person = {
+	name : "Chinedu",
+	age : 16,
+	gender : "SheMale",
+	password : "34567"
+};
+
+//update
+person.color = "black";
+person.password = "newpassword1234";
+
+
+//delete
+delete person.name
+
+
+console.log(Object.values(person));
+
+
+//read
+//console.log(person)
+
+//process.stdout.write(person)
+
