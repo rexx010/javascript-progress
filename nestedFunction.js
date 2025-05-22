8bb749b22d3f60df84f0b703d9fb28723854460a
rexx010 <grexx200@gmail.com>
2025-05-23 00:12:12 +0100

    created a nested function in js

---
+++ nestedFunction.js
@@ -0,0 +1,19 @@
+let numberOne = 3;
+let numberTwo = 4;
+
+
+function getProduct(number, second){
+	let numberThree = 2;
+
+	function multiply(value){
+		let product = value * number * second
+		return product;
+
+};
+return multiply(numberThree)
+};
+
+
+
+
+console.log(getProduct(numberOne, numberTwo))
