6d0da26378e5285a503f5a76421313ada0419dc0
rexx010 <grexx200@gmail.com>
2025-08-04 12:36:11 +0100

    Update ATMFunction.java

--- phasegate5/ATMFunction.java
+++ phasegate5/ATMFunction.java
@@ -20,14 +20,55 @@ validate4[0] = "Valid: true, issuer: American Express";
 
 if(atm[0] < 3 || atm[0] > 6){
 System.out.println(Arrays.toString(validate));
+
 }else if(atm[0] == 4){
-System.out.println(Arrays.toString(validate1));
+	System.out.println("Visa card");
+
+	int secondDigits = 0;
+	int oddPlace = 0;
+	int checker = 0;
+	for(int count = 0; count < atm.length; count+2){
+		checker += atm[count] * 2;
+		int mod = 0;
+		int div = 0;
+		if(checker > 9){
+			mod = checker % 10;
+			div = checker / 10;
+			secondDigits += mod + div;
+		}else{
+			secondDigits += checker;
+		}
+	}
+	
+	for(int counter = 1; counter < atm.length; counter+2){
+		oddPlace += atm[counter];
+	}
+	int total = secondDigits + oddPlace;
+	
+	if(total % 10 == 0){
+		for(int number = 0; number < atm.length; number++){
+		System.out.print("Credit Card Number: "+atm[number]);
+		}
+	System.out.print("Credit Card Digits: "+num1);
+	System.out.print("Credit Card Validity Status: Valid");
+
+	}else{
+	for(int number = 0; number < atm.length; number++){
+		System.out.print("Credit Card Number: "+atm[number]);
+		}
+	System.out.print("Credit Card Digits: "+num1);
+	System.out.print("Credit Card Validity Status: Invalid");
+
+	}
+
 }else if(atm[0] == 5){
-System.out.println(Arrays.toString(validate2));
+System.out.println("Master card");
+
 }else if(atm[0] == 6){
-System.out.println(Arrays.toString(validate3));
-}else if(num1 == 15 && atm[0] == 3){
-System.out.println(Arrays.toString(validate4));
+System.out.println("Discover card");
+
+}else if(atm[0] == 3 && atm[1] == 7){
+System.out.println("American Express card");
 }
 
 
