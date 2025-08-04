95b52aff0d3eff62c046955e6f103bca877f53b9
rexx010 <grexx200@gmail.com>
2025-08-04 12:46:41 +0100

    Create SecondHighestTest.java

---
+++ old works/SecondHighestTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SecondHighestTest{
+
+
+
+	@Test
+	public void functionExists(){
+
+	int[] number = {9, 6, 8, 11, 12};
+
+	SecondHighest.second(number);
+}
+
+
+/*	@Test
+	public void getSecond(){
+
+	int[] number = {9, 6, 8, 11, 12};
+
+	int expected = 11;
+
+	int output = SecondHighest.second(number);
+	assertEquals(expected, output);
+}
+*/
+
+}
